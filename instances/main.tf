3451e9552726a01f525ca7e5eb480a36ff390f03
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-01-22 13:45:22 -0800

    Fix security group name

--- instances/main.tf
+++ instances/main.tf
@@ -97,7 +97,7 @@ resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_public.id
-  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+  vpc_security_group_ids      = [aws_security_group.allow_iperf3.id]
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
 
