bbd4602961d157dcdae2f3e988ab4d1ca26b4542
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-01-22 21:44:54 -0800

    Output dest path

--- run_world_iperf3.sh
+++ run_world_iperf3.sh
@@ -40,6 +40,7 @@ for region in "${regions[@]}"; do
     region_name="${region_no_spaces//[^[:alnum:]_]/}" # Remove special charas except underscores
 
     dest_path="${dest_fold}/${region_name}"
+    echo "Saving to dir $dest_path"
     mkdir "${dest_path}"
     name_down="${dest_path}/${region}_throughput_client_p4_down"
     name_up="${dest_path}/${region}_throughput_client_p4_up"
