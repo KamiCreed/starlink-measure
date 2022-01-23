34427ea6bd627364f9a97aaf93e430ee74042e50
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-01-22 20:47:12 -0800

    Update script for deployment

--- run_world_iperf3.sh
+++ run_world_iperf3.sh
@@ -22,14 +22,14 @@ unique_fname() {
 run_iperf() {
     fname_down="$1"
     fname_up="$2"
-    length=10
+    length=600
 
     iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down" & 
     iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up"
 }
 
-#regions=(ap-southeast-2 ap-southeast-1 ap-northeast-1 eu-west-2 us-west-1)
-regions=(ap-southeast-2 us-west-1)
+regions=(ap-southeast-2 ap-southeast-1 ap-northeast-1 ap-south-1 eu-west-2 me-south-1 sa-east-1 us-west-1)
+#regions=(ap-southeast-2 us-west-1)
 
 for region in "${regions[@]}"; do 
     (cd instances; terraform apply -auto-approve -var "region=$region" || true)
@@ -46,7 +46,11 @@ for region in "${regions[@]}"; do
         echo "Sleeping and trying again..."
         sleep 30
     done
-    fg || true # Just in case the first one lags behind
+    fg 2>/dev/null || true # Just in case the first one lags behind
+
+    echo "Logged measurements to the following:"
+    echo "$fname_down"
+    echo "$fname_up"
 
     (cd instances; terraform destroy -auto-approve -var "region=$region")
 done
