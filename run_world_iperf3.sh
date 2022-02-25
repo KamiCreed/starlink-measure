3f1be0c03499373fa8268af08900688c47e0760d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-25 02:31:45 -0800

    Use subshell to return iperf on first error

--- run_world_iperf3.sh
+++ run_world_iperf3.sh
@@ -23,23 +23,25 @@ unique_fname() {
 }
 
 run_iperf() {
-    set -e
-
     fname_down="$1"
     fname_up="$2"
     fname_down_udp="$3"
     fname_up_udp="$4"
     length=$5
 
-    # TCP
-    iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down" & 
-    iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up"
-    wait
+    (
+        set -e # Subshell to exit on first error
 
-    # UDP
-    iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down_udp" & 
-    iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up_udp"
-    wait
+        # TCP
+        iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down" & 
+        iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up"
+        wait
+
+        # UDP
+        iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down_udp" & 
+        iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up_udp"
+        wait
+    )
 }
 
 regions=(ap-southeast-2 ap-southeast-1 ap-northeast-1 ap-south-1 eu-west-2 me-south-1 sa-east-1 us-west-1)
@@ -74,7 +76,6 @@ for region in "${regions[@]}"; do
         sleep 30
         echo "Starting..."
     done
-    wait || true # Just in case the first one lags behind
 
     echo "Logged measurements to the following:"
     echo "$fname_down"
