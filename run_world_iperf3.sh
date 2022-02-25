f17ea95b6b69e741b39fbbb889486d34ec1bbeff
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-02-25 01:53:12 -0800

    Add UDP

--- run_world_iperf3.sh
+++ run_world_iperf3.sh
@@ -6,7 +6,9 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 dest_fold="$1"
-length=600
+length=30
+CLIENT=client
+SERVER=server
 
 unique_fname() {
     count=1
@@ -21,12 +23,23 @@ unique_fname() {
 }
 
 run_iperf() {
+    set -e
+
     fname_down="$1"
     fname_up="$2"
-    length=$3
+    fname_down_udp="$3"
+    fname_up_udp="$4"
+    length=$5
 
+    # TCP
     iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down" & 
     iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up"
+    wait
+
+    # UDP
+    iperf3 -c "$instance_ip" -R -Z -t $length -P 4 -J > "$fname_down_udp" & 
+    iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up_udp"
+    wait
 }
 
 regions=(ap-southeast-2 ap-southeast-1 ap-northeast-1 ap-south-1 eu-west-2 me-south-1 sa-east-1 us-west-1)
@@ -48,19 +61,20 @@ for region in "${regions[@]}"; do
     dest_path="${dest_fold}/${region_name}"
     echo "Saving to dir $dest_path"
     mkdir -p "${dest_path}"
-    name_down="${dest_path}/${region}_throughput_client_p4_down"
-    name_up="${dest_path}/${region}_throughput_client_p4_up"
+    name="${dest_path}/${region}_throughput_client_p4"
 
     echo "Attempting to run iperf3 for $length seconds"
 
-    fname_down="$(unique_fname $name_down)"
-    fname_up="$(unique_fname $name_up)"
-    until run_iperf "$fname_down" "$fname_up" $length; do
+    fname_down="$(unique_fname $name)_down"
+    fname_up="$(unique_fname $name)_up"
+    fname_down_udp="$(unique_fname $name)_down_udp"
+    fname_up_udp="$(unique_fname $name)_up_udp"
+    until run_iperf "$fname_down" "$fname_up" "$fname_down_udp" "$fname_up_udp" $length; do
         echo "Error. Sleeping and trying again..."
         sleep 30
         echo "Starting..."
     done
-    fg 2>/dev/null || true # Just in case the first one lags behind
+    wait || true # Just in case the first one lags behind
 
     echo "Logged measurements to the following:"
     echo "$fname_down"
