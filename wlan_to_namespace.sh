79a7c4ee6b683c14fcd8d1f8bc1846c377c93f3d
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-03-21 13:01:25 -0700

    Use wifi way of moving to namespace

--- wlan_to_namespace.sh
+++ wlan_to_namespace.sh
@@ -2,7 +2,10 @@ ip netns add net_remote_ns
 
 # Set namespace to use wlan0, after this point wlan0 is not usable by programs
 # outside the namespace
-ip link set wlan0 netns net_remote_ns
+iw phy phy0 set netns "$(ip netns exec net_remote_ns sh -c 'sleep 1 >&- & echo "$!"')"
+
+# This one only works for ethernet
+#ip link set wlan0 netns net_remote_ns
 
 ip netns exec net_remote_ns ip link set wlan0 up
 
