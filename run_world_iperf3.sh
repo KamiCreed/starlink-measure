609e112b5a1c5a6b7b73caa9aeaa58d071816fab
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-03-25 10:04:07 -0700

    Test one TCP before going into UDP

--- run_world_iperf3.sh
+++ run_world_iperf3.sh
@@ -40,6 +40,9 @@ run_iperf() {
     #iperf3 -c "$instance_ip" -p 5202 -Z -t $length -P 4 -J > "$fname_up"
     #wait
 
+    echo "Test one sec TCP"
+    iperf3 -c "$instance_ip" -R -Z -t 1
+
     echo "Running UDP measurements"
     iperf3 -c "$instance_ip" -R -Z -t $length -u -b 57M -P 16 -J > "$fname_down_udp" & 
     iperf3 -c "$instance_ip" -p 5202 -Z -t $length -u -b 38M -P 4 -J > "$fname_up_udp"
