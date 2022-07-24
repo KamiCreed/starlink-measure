9cd996838e9fdcb47038a2a7c1830865995faddc
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2022-07-24 04:53:01 -0700

    Add packet test

---
+++ run_packet_test.sh
@@ -0,0 +1,12 @@
+(cd instances; terraform apply -auto-approve)
+echo "running"
+instance_ip="$(cd instances; terraform output -raw us-west-1_public_ip)"
+down=210
+
+./run_world_iperf3.sh -n -d ${down}M -u ../packet_full_2022-07-23_starlink_iperf3
+./run_world_iperf3.sh -n -d $(((${down}+1)/2))M -u ../packet_half_2022-07-23_starlink_iperf3
+./run_world_iperf3.sh -n -d $(((${down}+2)/3))M -u ../packet_third_2022-07-23_starlink_iperf3
+./run_world_iperf3.sh -n -d $(((${down}+3)/4))M -u ../packet_quarter_2022-07-23_starlink_iperf3
+./run_world_iperf3.sh -n -d $(((${down}+4)/5))M -u ../packet_fifth_2022-07-23_starlink_iperf3
+
+(cd instances; terraform destroy -auto-approve)
