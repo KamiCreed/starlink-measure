a6f2ee42caa3d9febd2c1987653af1979c05490f
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-05-30 12:45:58 -0700

    Complete initial pantheon script

--- pantheon_scripts/run_pantheon_starlink.sh
+++ pantheon_scripts/run_pantheon_starlink.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-# Inside `pantheon` folder
+pantheon_dir="$1"
+data_dir="$2"
+ssh_cmd="$3"
 
-src/experiments/test.py remote --all
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <pantheon_dir> <data_dir> <ssh_cmd>" >&2
+    exit 2
+fi
+
+cd "$pantheon_dir"
+
+src/experiments/test.py remote --data-dir "$data_dir" --all $ssh_cmd
+src/experiments/analyze.py --data-dir "$data_dir"
