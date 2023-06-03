4183986f684254d69a81bfd1ee8ac90a9391036e
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-06-02 21:22:15 -0700

    Add sender

--- pantheon_scripts/run_pantheon_starlink.sh
+++ pantheon_scripts/run_pantheon_starlink.sh
@@ -3,14 +3,15 @@
 pantheon_dir="$1"
 data_dir="$2"
 ssh_cmd="$3"
+sender="$4"
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <pantheon_dir> <data_dir> <ssh_cmd>" >&2
+    echo "Usage: $0 <pantheon_dir> <data_dir> <ssh_cmd> <sender>" >&2
     exit 2
 fi
 
 cd "$pantheon_dir"
 
 # 20 runs of 30 seconds with 5 minute rest
-src/experiments/test.py remote -t 30 -f 4 --run-times 20 --data-dir "$data_dir" --all $ssh_cmd
+src/experiments/test.py remote --sender $sender -t 30 -f 4 --run-times 20 --data-dir "$data_dir" --all $ssh_cmd
 src/analysis/analyze.py --data-dir "$data_dir"
