5c8b585845a771c9d47033c4eacbeca8c02965a4
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-06-04 00:32:31 -0700

    Fix data dir

--- pantheon_scripts/run_pantheon_starlink.sh
+++ pantheon_scripts/run_pantheon_starlink.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 pantheon_dir="$1"
 data_dir="$2"
 ssh_cmd="$3"
@@ -18,8 +20,8 @@ schemes=(cubic bbr)
 for scheme in "${schemes[@]}"; do
     for i in {1..20}; do
         spec_data_dir="${data_dir}_${scheme}_test${i}"
-        src/experiments/test.py remote --sender $sender -t 30 -f 4 --run-times 20 --data-dir "$data_dir" --schemes "${scheme}" $ssh_cmd
-        src/analysis/analyze.py --data-dir "$data_dir"
+        src/experiments/test.py remote --sender $sender -t 30 -f 4 --run-times 20 --data-dir "$spec_data_dir" --schemes "${scheme}" $ssh_cmd
+        src/analysis/analyze.py --data-dir "$spec_data_dir"
         sleep 300
     done
 done
